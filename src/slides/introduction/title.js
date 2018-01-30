63787cb14c8c29f27303a182319dc1a9226de671
Michael Peyper <michael.peyper@ioof.com.au>
2018-01-30 22:13:35 +1100

    Alternative title images

--- src/slides/introduction/title.js
+++ src/slides/introduction/title.js
@@ -3,14 +3,14 @@ import { Slide, Heading, Image, Text, Link } from 'spectacle'
 import Twitter from 'react-icons/lib/fa/twitter'
 import Github from 'react-icons/lib/fa/github'
 import Envelope from 'react-icons/lib/fa/envelope'
-import microFrontendsImage from './images/micro-frontends.png'
+import microFrontendsImage from './images/micro-frontends-colourful.png'
 
 export default (
   <Slide bgColor="primary">
     <Heading fit textFont="primary" textColor="secondary">
       From Monolith to Micro-Frontends
     </Heading>
-    <Image src={microFrontendsImage} width="80%" />
+    <Image src={microFrontendsImage} width="30%" />
     <Text textAlign="right">
       <Link textFont="secondary" textColor="quartenary" href="https://twitter.com/michaelpeyper" target="_blank">
         @michaelpeyper
