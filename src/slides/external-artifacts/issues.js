f05202d15bd266bd080f1a4d49d0e466bf236acc
Michael Peyper <michael.peyper@ioof.com.au>
2018-01-31 16:40:15 +1100

    Clean up and notes

--- src/slides/external-artifacts/issues.js
+++ src/slides/external-artifacts/issues.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Slide, Heading, List, ListItem } from 'spectacle'
+import { Slide, Heading, List, ListItem, Notes } from 'spectacle'
 
 export default (
   <Slide>
-    <Heading fit textFont="primary" textColor="secondary">
-      Complications
+    <Heading textFont="primary" textColor="secondary">
+      Issues
     </Heading>
     <List ordered textFont="secondary" textColor="quartenary">
       <ListItem>Releases</ListItem>
       <ListItem>Bundle Size</ListItem>
       <ListItem>Assets</ListItem>
     </List>
+    <Notes>
+      Hard to tell what is being released<br />
+      Bundle size blows out over extra frameworks, duplicate packages and webpack in general<br />
+      How to deal with assets for the components?<br />
+    </Notes>
   </Slide>
 )
